c75db81dfd213ea9dbf7db04c3a0ff2fcc3d1b0e
ShahinAliyev <sahin.aliyev979@gmail.com>
2025-08-22 12:03:27 +0200

    FIrst Commit

---
+++ src/test/java/com/basickafka/kafkafirst/KafkaFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basickafka.kafkafirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
